d627ed21e924a03a1b7b63123ff4831932687c11
Mary-Ann Horley <maryann.horley@gmail.com>
2020-02-18 17:02:12 +0000

    added postcode validation

--- app.py
+++ app.py
@@ -1,28 +1,52 @@
+
 from flask import Flask, render_template, redirect, session, flash, request, jsonify, url_for, Blueprint
 import requests
 from flask_bootstrap import Bootstrap
 from datetime import datetime, timedelta, date
 import calendar
 import pprint
+from forms import Report
+from weasyprint import HTML
 
 app = Flask(__name__)
+flaskpdf.init_app(app)
 app.config.from_pyfile('settings.py')
 pp = pprint.PrettyPrinter(indent=4)
 
 bootstrap = Bootstrap(app) 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    form = Report()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            postcode = form.data.postcode
+            email = form.data.email
+            validate_url = "api.postcodes.io/postcodes/" + postcode + "/validate"
+            r = requests.get(validate_url)
+            data = r.json()
+            if data["result"] == "true":
+                geocode_url = "https://api.postcodes.io/postcodes/" + postcode
+                r = requests.get(geocode_url)
+                data = r.json()
+                lat = data["latitude"]
+                long = data["longitude"]
+                month = form.data.month
+                year = form.data.year
+                report_url = str(url_for("weather")) + "/" + month + "/" + year + "/" + lat + "/" + long
+                filename = postcode + "_" + month + "_" + year + ".pdf"
+                pdf = HTML(report_url).write_pdf(filename)
+            else:
+                pass
+                # if the postcode doesn't validate go back to index with a flashed message
+    return render_template("index.html", form=form)
 
-@app.route("/<monthyear>")
-def weather(monthyear):
+@app.route("/<monthyear>/<lat>/<long>")
+def weather(monthyear, lat, long):
     month = monthyear[:2]
     month_name = calendar.month_name[int(month)]
     year = monthyear[-4:]
     api_key = app.config["DARKSKY"]
-    latitude = str(app.config["LAT"])
-    longitude = str(app.config["LONG"])
     payload = {'exclude': 'currently,minutely,hourly,alerts,flags', 'units': 'uk2'}
     days = calendar.monthrange(int(year), int(month))
     filler_days = 7
@@ -36,7 +60,7 @@ def weather(monthyear):
         day = str(i+1).zfill(2)
         date = year + "-" + month + "-" + day + "T12:00:00Z"
         dt = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
-        api_url = "https://api.darksky.net/forecast/" + api_key + "/" + latitude + "," + longitude + "," + date
+        api_url = "https://api.darksky.net/forecast/" + api_key + "/" + lat + "," + long + "," + date
         r = requests.get(api_url, params=payload)
         data = r.json()
         day_name = dt.strftime("%A")
